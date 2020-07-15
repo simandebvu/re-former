5a66d2907cb0e90736fada8b556336ae923b27ae
Shingirayi Mandebvu <shingim1@gmail.com>
2020-07-15 23:49:06 +0200

    Fix Rubocop Offenses

--- Gemfile
+++ Gemfile
@@ -4,8 +4,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.6'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 gem 'figaro'
+gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
@@ -31,7 +31,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
 end
 
 group :development do
@@ -48,4 +48,4 @@ group :test do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,44 +1,46 @@
 class UsersController < ApplicationController
-    def index
-      @users = User.all
-    end
-    def new
+  def index
+    @users = User.all
+  end
+
+  def new
     @user = User.new
-    end
+  end
 
-    def edit
+  def edit
     @user = User.find(params[:id])
-    end
+  end
 
-    def show
+  def show
     @user = User.find(params[:id])
-    end
+  end
 
-    def create
+  def create
     @user = User.new(user_params)
 
     if @user.save
-    redirect_to @user
+      redirect_to @user
     else
-    render :new
+      render :new
     end
-    end
+  end
 
-    def destroy
-      @user = User.find(params[:id])
-      @user.destroy
-      redirect_to "#index"
-    end
-    
-    def update
-      @user = User.find(params[:id])
-      @user.update(user_params)
-      flash.notice = "User '#{@user.username}' Updated!"
-      redirect_to user_path(@user)
-    end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to '#index'
+  end
 
-    private
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    flash.notice = "User '#{@user.username}' Updated!"
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
